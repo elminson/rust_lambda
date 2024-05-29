de200524511d469f8ca69981dfce7cc873496ffa
Elminson De Oleo Baez <edeoleo@gmail.com>
2024-05-29 11:05:42 -0400

    Add tracing to lambda runtime for CloudWatch error logging

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,76 @@
-use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
+// This example requires the following input to succeed:
+// { "command": "do something" }
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+use lambda_http::lambda_runtime;
+use lambda_runtime::{service_fn, tracing, Error, LambdaEvent};
+use serde::{Deserialize, Serialize};
 
+// This is also a made-up example. Requests come into the runtime as unicode
+// strings in json format, which can map to any structure that implements `serde::Deserialize`
+// The runtime pays no attention to the contents of the request payload.
+#[derive(Deserialize)]
+struct Request {
+    command: String,
+    name: String,
+}
 
-    println!("message: {}", &message);
-
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
+// This is a made-up example of what a response structure may look like.
+// There is no restriction on what it can be. The runtime requires responses
+// to be serialized into json. The runtime pays no attention
+// to the contents of the response payload.
+#[derive(Serialize, Debug)]
+struct Response {
+    req_id: String,
+    msg: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    // required to enable CloudWatch error logging by the runtime
     tracing::init_default_subscriber();
 
-    run(service_fn(function_handler)).await
+    let func = service_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+pub(crate) async fn my_handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
+    // extract some useful info from the request
+    let command = event.payload.command;
+    let name = event.payload.name;
+
+    // prepare the response
+    let resp = Response {
+        req_id: event.context.request_id,
+        msg: format!("Hi {}, Command {} executed.", name, command),
+    };
+    // log the response
+    tracing::info!("Response: {:?}", resp);
+    // return `Response` (it will be serialized to JSON automatically by the runtime)
+    Ok(resp)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{my_handler, Request};
+    use lambda_runtime::{Context, LambdaEvent};
+
+    #[tokio::test]
+    async fn response_is_good_for_simple_input() {
+        let id = "ID";
+
+        let mut context = Context::default();
+        context.request_id = id.to_string();
+
+        let payload = Request {
+            command: "X".to_string(),
+            name: "".to_string(),
+        };
+        let event = LambdaEvent { payload, context };
+
+        let result = my_handler(event).await.unwrap();
+
+        assert_eq!(result.msg, "Command X executed.");
+        assert_eq!(result.req_id, id.to_string());
+    }
 }
